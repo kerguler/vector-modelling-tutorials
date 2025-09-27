5b34fd4ecc035a864c5b22d64f14595705511194
Kamil Erguler <kerguler@gmail.com>
2025-09-27 17:30:07 +0300

    test

--- init_tutorials.sh
+++ init_tutorials.sh
@@ -8,4 +8,18 @@ fi
 # ensure jovyan owns its full home (covers .local too)
 chown -R jovyan:users /home/jovyan
 
+chmod +x /home/jovyan/tutorials/mina/shiny-wrapper.R
+cat >> /home/jovyan/.jupyter/jupyter_server_config.py <<'EOF'
+c.ServerProxy.servers = {
+    'shiny': {
+        'command': ['Rscript', '/home/jovyan/tutorials/mina/shiny-wrapper.R'],
+        'timeout': 3000,
+        'launcher_entry': {
+            'title': 'Shiny App',
+            'icon_path': ''  # you can give a PNG/SVG path if you like
+        }
+    }
+}
+EOF
+
 exec start.sh "$@"
