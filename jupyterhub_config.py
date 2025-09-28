2a9931b63e5f888d20cf558f2109ece1371e15c3
Kamil Erguler <kerguler@gmail.com>
2025-09-29 01:02:54 +0300

    quota

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -60,6 +60,12 @@ if shared_tutorials_folder:
 c.Spawner.mem_limit = "4G"
 c.Spawner.cpu_limit = 2
 
+c.DockerSpawner.extra_host_config = {
+    "storage_opt": {
+        "size": "5G"
+    }
+}
+
 # allow more time for user containers to come online
 c.Spawner.http_timeout = 120      # wait up to 120s for JupyterLab to reply
 c.Spawner.start_timeout = 180     # allow 3 minutes total for startup
