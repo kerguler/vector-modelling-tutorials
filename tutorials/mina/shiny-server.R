38c783f7d9cb5581eb0890baa76fcc07d66496d0
Kamil Erguler <kerguler@gmail.com>
2025-09-28 08:14:38 +0300

    fixed

--- tutorials/mina/shiny-server.R
+++ tutorials/mina/shiny-server.R
@@ -4,14 +4,16 @@ suppressPackageStartupMessages({
   library(shiny)
 })
 
+filename <- "pinn-shinyv4V5.R"
+
 # Read port/host from jupyter-server-proxy
 port <- as.numeric(Sys.getenv("PORT", "8050"))
 host <- Sys.getenv("HOST", "127.0.0.1")
 
 # Load your app definition (must define ui and server)
-source("/home/jovyan/tutorials/mina/pinn-shinyv4V5.R")
+source(sprintf("./%s", filename))
 
-message(sprintf("Starting Shiny app from pinn-shinyv4V5.R on %s:%d", host, port))
+message(sprintf("Starting Shiny app from %s on %s:%d", filename, host, port))
 
 shiny::shinyApp(
   ui = ui,
