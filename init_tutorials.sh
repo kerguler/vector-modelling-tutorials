70ddbd59636cd1b2101c9d4611cf92bdd7a1d4fd
Kamil Erguler <kerguler@gmail.com>
2025-09-29 00:12:07 +0300

    test

---
+++ init_tutorials.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ ! -d /home/jovyan/tutorials ]; then
+  # cp -r /srv/tutorials /home/jovyan/tutorials
+  ln -s /srv/tutorials /home/jovyan/tutorials
+fi
+
+# ensure jovyan owns its full home (covers .local too)
+chown -R jovyan:users /home/jovyan
+
+exec start.sh "$@"
