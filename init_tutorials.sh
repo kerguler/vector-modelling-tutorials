388bd07dee5ff44c81ec1bf551a3387ac5149a5b
Kamil Erguler <kerguler@gmail.com>
2025-10-02 17:41:31 +0300

    should be fixed

--- init_tutorials.sh
+++ init_tutorials.sh
@@ -5,7 +5,6 @@ if [ ! -d /home/jovyan/tutorials ]; then
   ln -s /srv/tutorials /home/jovyan/tutorials
 fi
 
-# ensure jovyan owns its full home (covers .local too)
 chown -R jovyan:users /home/jovyan
 
 exec start.sh "$@"
