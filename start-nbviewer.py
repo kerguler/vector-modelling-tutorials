ef4848a9c97824fe568a11734cbc666433564632
Kamil Erguler <kerguler@gmail.com>
2025-11-11 10:36:39 +0200

    draft

---
+++ start-nbviewer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import cgi, html, runpy, sys
+setattr(cgi, "escape", html.escape)
+
+sys.argv = [
+    "nbviewer",
+    "--localfiles=/srv/tutorials",
+    "--localfile-path=/srv/tutorials",
+    "--base-url=/tutorials-viewer",
+    "--port=80",
+    "--no-cache",
+    "--jupyterhub-url=https://veclim.com/tutorials",
+]
+
+runpy.run_module("nbviewer", run_name="__main__")
